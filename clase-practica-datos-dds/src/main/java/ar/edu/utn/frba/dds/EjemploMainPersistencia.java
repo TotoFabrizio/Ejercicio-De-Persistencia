fa11a3acbf0ce999e68a149dd155e60f4d1e6b72
TotoFabrizio <tomas.fabrizio02@gmail.com>
2024-09-18 21:12:06 -0300

    Arreglando que subi mal el ejercicio de persistencia

--- clase-practica-datos-dds/src/main/java/ar/edu/utn/frba/dds/EjemploMainPersistencia.java
+++ clase-practica-datos-dds/src/main/java/ar/edu/utn/frba/dds/EjemploMainPersistencia.java
@@ -1,5 +1,7 @@
 package ar.edu.utn.frba.dds;
 
+import ar.edu.utn.frba.dds.ejercicio_01.repositorios.Repositorio;
+
 public class EjemploMainPersistencia {
     
     public static void main(String[] args) {
--- clase-practica-datos-dds/src/main/java/ar/edu/utn/frba/dds/ejercicio_01/Ejercicio.java
+++ clase-practica-datos-dds/src/main/java/ar/edu/utn/frba/dds/ejercicio_01/Ejercicio.java
@@ -3,9 +3,19 @@ package ar.edu.utn.frba.dds.ejercicio_01;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
-public class Ejercicio {
+@Entity
+@Table(name = "ejercicio")
+public class Ejercicio extends Persistente{
+
+    @Column(name = "nombre")
     private String nombre;
+
+    @Column(name = "detalle")
     private String detalle;
 }
